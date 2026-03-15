ec4de6c4f263b6bcd4444376782038d9bf603d59
Sadiya <160082120+sadiyameem@users.noreply.github.com>
2026-03-15 14:35:00 -0400

    Added selectors, event objects, draw and erase false, detected touch device, and created grid

---
+++ script.js
@@ -0,0 +1,81 @@
+let container = document.querySelector(".container");
+let gridButton = document.querySelector("submit-grid");
+let clearGridButton = document.querySelector("clear-grid");
+let gridWidth = document.querySelector("width-range");
+let gridHeight = document.querySelector("height-range");
+let colorButton = document.querySelector("color-input");
+let erasebtn = document.querySelector("erase-btn");
+let paintBtn = document.querySelector("paint-btn");
+let widthValue = document.querySelector("width-value");
+let heightValue = document.querySelector("height-value");
+
+let events = {
+    mouse: {
+        down: "mousedown",
+        move: "mousemove",
+        up: "mouseup",
+    },
+    touch: {
+        down: "touchstart",
+        move: "touchmove",
+        up: "touchand",
+    },
+};
+
+let deviceType = "";
+
+let draw = false;
+let erase = false;
+
+// detect touch device
+const isTouchDevice = () => {
+    try {
+        document.createEvent("TouchEvent");
+        deviceType = "touch";
+        return true;
+    } catch (e) {
+        deviceType = "mouse";
+        return false;
+    }
+};
+
+isTouchDevice();
+
+// create grid
+gridButton.addEventListener("click", () => {
+    container.innerHTML = "";
+    let count = 0;
+    for (let i = 0; i < gridHeight.ariaValueMax; i++) {
+        count += 2;
+        let div = document.createElement("div");
+        div.classList.add("gridRow");
+        for (let j = 0; j < gridWidth.ariaValueMax; j++) {
+            count += 2;
+            let col = document.createElement("div");
+            col.classList.add("gridCol");
+            col.setAttribute("id", `gridCol${count}`);
+
+            col.addEventListener(events[deviceType].down, () => {
+                draw = true;
+                if (erase) {
+                    col.style.backgroundColor = "transparent";
+                } else {
+                    col.style.backgroundColor = colorButton.ariaValueMax;
+                }
+            });
+
+            col.addEventListener(events[deviceType].move, (e) => {
+                let elementId = document.elementFromPoint (
+                    !isTouchDevice() ? e.clientX : e.touches[0].clientX,
+                    !isTouchDevice() ? e.clientY : e.touches[0].clientY
+                ).id;
+                checker(elementId);
+            });
+            col.addEventListener(events[deviceType].up, () => {
+                draw = false;
+            });
+            div.appendChild(col);
+        }
+        container.appendChild(div);
+    }
+});
