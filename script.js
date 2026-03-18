86d86043a03cda2640ba71635c37cb0210f5399b
Sadiya <160082120+sadiyameem@users.noreply.github.com>
2026-03-18 17:01:18 -0400

    created event objects which include the draw and erase that is false and touch device detect

--- script.js
+++ script.js
@@ -8,3 +8,35 @@ let eraseBtn = document.getElementById("erase-btn");
 let paintBtn = document.getElementById("paint-btn");
 let widthValue = document.getElementById("width-value");
 let heightValue = document.getElementById("height-value");
+
+// create event object
+let events = {
+    mouse: {
+        down: "mousedown",
+        move: "mousemove",
+        up: "mouseup",
+    },
+    touch: {
+        down: "touchstart",
+        move: "touchmove",
+        up: "touchend",
+    },
+};
+let deviceType = "";
+
+// draw and erase would be false
+let draw = false;
+let erase = false;
+
+// detect touch device
+const isTouchDevice = () => {
+    try {
+        document.createElement("TouchEvent");
+        deviceType = "touch";
+        return true;
+    } catch (e) {
+        deviceType = "mouse";
+        return false;
+    }
+};
+isTouchDevice();
